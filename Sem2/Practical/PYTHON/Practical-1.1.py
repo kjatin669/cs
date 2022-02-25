6dfea652e44f0e88e9d1d04528739fd00d41ad68
Jatin Gohil <gohiljatin659@gmail.com>
2022-02-25 19:21:59 +0530

    Initial Commit

---
+++ Sem2/Practical/PYTHON/Practical-1.1.py
@@ -0,0 +1,22 @@
+# Practical 1.1
+
+# Topic of Practical
+
+def getMin(array, n):
+    resMin = array[0]
+    for i in range(1, n):
+        resMin = min(array[i], resMin)
+    return resMin
+
+def getMax(array, n):
+    resMax = array[0]
+    for i in range(1, n):
+        resMax = min(array[i], resMax)
+    return resMax
+
+array = [4, 45, 32, 42, 94, 21, 43, 11]
+n = len(array)
+
+print(array)
+print("The Minimum value of Array is", getMin(array, n))
+print("The Maximum Value of Array is", getMax(array, n))
